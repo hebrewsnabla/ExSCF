0984f18893e02d4c9da62ee19bc7af7c105f6f4f
hebrewsnabla <srwang20@fudan.edu.cn>
2021-02-10 22:41:55 +0800

    dump tot NO to NO.fch

--- pyphf/util2.py
+++ pyphf/util2.py
@@ -22,16 +22,19 @@ def reg2ortho(dm, X, forward=True):
 def tofch(oldfch, natorb, natocc, S, flag):
     fch = oldfch.split('.fch')[0] + '_' + flag + '.fch'
     os.system('cp %s %s' % (oldfch, fch))
+    os.system('fch_u2r %s' % fch)
     #S = suhf.mol.intor_symmetric('int1e_ovlp')
     Sdiag = S.diagonal()
-    natorb_a, natorb_b = natorb
-    natocc_a, natocc_b = natocc
-    nbfa = natorb_a.shape[0]
-    nifa = natorb_a.shape[1]
-    py2fch(fch, nbfa, nifa, natorb_a, Sdiag, 'a', natocc_a)
-    nbfb = natorb_b.shape[0]
-    nifb = natorb_b.shape[1]
-    py2fch(fch, nbfb, nifb, natorb_b, Sdiag, 'b', natocc_b)
+    #natorb_a, natorb_b = natorb
+    #natocc_a, natocc_b = natocc
+    #nbfa = natorb_a.shape[0]
+    #nifa = natorb_a.shape[1]
+    nbf = natorb.shape[0]
+    nif = natorb.shape[1]
+    py2fch(fch, nbf, nif, natorb, Sdiag, 'a', natocc)
+    #nbfb = natorb_b.shape[0]
+    #nifb = natorb_b.shape[1]
+    #py2fch(fch, nbfb, nifb, natorb_b, Sdiag, 'b', natocc_b)
 
 def dump_moe(moe, na, nb):
     ea = moe[0]
