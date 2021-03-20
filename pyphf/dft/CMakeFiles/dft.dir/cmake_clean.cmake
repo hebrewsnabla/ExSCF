59b111ec7372844bfcef86304cbcabf6c5bc9193
hebrewsnabla <srwang20@fudan.edu.cn>
2021-03-20 22:31:12 +0800

    move xc_itrf

---
+++ pyphf/dft/CMakeFiles/dft.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dft.dir/CxLebedevGrid.c.o"
+  "CMakeFiles/dft.dir/grid_basis.c.o"
+  "CMakeFiles/dft.dir/nr_numint.c.o"
+  "CMakeFiles/dft.dir/r_numint.c.o"
+  "CMakeFiles/dft.dir/numint_uniform_grid.c.o"
+  "../libdft.pdb"
+  "../libdft.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
