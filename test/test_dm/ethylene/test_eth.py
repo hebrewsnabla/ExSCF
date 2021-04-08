5ed860471152681f418e2efece5c137723e1fab0
hebrewsnabla <srwang20@fudan.edu.cn>
2021-04-08 22:45:17 +0800

    update test

--- test/test_dm/ethylene/test_eth.py
+++ test/test_dm/ethylene/test_eth.py
@@ -1,10 +1,10 @@
 #import phf
-from pyscf import gto, scf, lib
+from pyscf import lib
 #import scipy
 #import numpy as np
-import sys
+#import sys
 
-from pyphf import util
+from pyphf import util, guess
 
 lib.num_threads(4)
 
@@ -13,13 +13,13 @@ fch = 'eth_uhf.fch' #sys.argv[2]
 bas = 'def2svp'
 
 
-mf = util.guess_from_fchk(xyz, bas, fch)
+mf = guess.from_fchk(xyz, bas, fch)
 
 mf2 = util.SUHF(mf)
 #mf2.cut_no = False
 #mf2.debug = False
 mf2.diis_on = True
-mf2.diis_start_cyc = 20
+#mf2.diis_start_cyc = 20
 mf2.max_cycle = 100
 mf2.tofch = True
 mf2.oldfch = 'eth_uhf.fch'
