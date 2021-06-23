49b9cb70e14c7ca580f1a15c282cb81de8ede6f7
hebrewsnabla <srwang20@fudan.edu.cn>
2021-06-23 20:17:18 +0800

    add mo_occ

---
+++ test/mom/test.py
@@ -0,0 +1,12 @@
+from pyphf import suscf, guess
+from pyscf import lib
+
+lib.num_threads(4)
+xyz = 'H 0.0 0.0 0.0; H 0.0 0.0 2.0'''
+bas = '3-21g'
+mf = guess.mix(xyz, bas, conv='tight')
+
+mf2 = suscf.SUHF(mf)
+mf2.verbose = 8
+mf2.kernel()
+
